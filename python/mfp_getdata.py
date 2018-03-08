adabeb3ccef61bf8f5fb2099253c6a37709f6aa1
NateDee <35738092+NateDee@users.noreply.github.com>
2018-03-08 21:53:46 +0000

    mfp_getdata into folder

--- python/mfp_getdata.py
+++ python/mfp_getdata.py
@@ -1,1 +1,24 @@
-holder
+from __future__ import print_function
+from jsobject import Object
+import sys, json
+import myfitnesspal
+import datetime
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+
+now = datetime.datetime.now()
+client = myfitnesspal.Client(user, passwd)
+current_stats = client.get_date(now.year, now.month, now.day)
+
+data_to_send = []
+#fiber sodium carbs cals fat protein
+for key in current_stats.totals:
+	data_to_send.append(current_stats.totals[key])
+
+for key in current_stats.goals:
+	data_to_send.append(current_stats.goals[key])
+
+#jso_data = Object(current_stats.totals)
+
+print(data_to_send)
